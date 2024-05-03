0f0bb188ffafcd8f1bbc855a2b43fa40c1ac2d4f
felipejoribeiro <feliperibeiro.ufu@gmail.com>
2024-05-03 19:44:58 -0300

    added point light to scene

--- src/lib/scene.svelte
+++ src/lib/scene.svelte
@@ -1,9 +1,33 @@
-<script>
+<script lang="ts">
 	import { T } from '@threlte/core';
 	import { OrbitControls, interactivity } from '@threlte/extras';
 	interactivity();
 
 	import Ant from './ant.svelte';
+
+	let light: {
+		intensity: number;
+		position: [number, number, number];
+	} = {
+		intensity: 29,
+		position: [0, 2, 0]
+	};
+
+	let sphere: {
+		position: [number, number, number];
+	} = {
+		position: [0, 2, 0]
+	};
+
+	function float() {
+		const timeInSeconds = Date.now() / 1000;
+		const offset = 2.9;
+		light.position = [Math.sin(timeInSeconds) * offset, 2, Math.cos(timeInSeconds) * offset];
+		sphere.position = [Math.sin(timeInSeconds) * offset, 2, Math.cos(timeInSeconds) * offset];
+		requestAnimationFrame(float);
+	}
+
+	float();
 </script>
 
 <T.PerspectiveCamera
@@ -19,4 +43,11 @@
 <Ant scale={4} position.y={-3} />
 
 <T.AmbientLight intensity={1} />
+<T.PointLight {...light} />
+
 <T.DirectionalLight color="red" intensity={1} position={[-1, 3, -1]} castShadow />
+
+<T.Mesh position={sphere.position}>
+	<T.SphereGeometry args={[0.05, 8, 8]} />
+	<T.MeshStandardMaterial color="white" emissiveIntensity={20} emissive="white" />
+</T.Mesh>
